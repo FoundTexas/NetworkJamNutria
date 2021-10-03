9122b8e29ab95ad35c0d44ee8f8f2551603af0d5
Cytanic <maxvalentino@hotmail.com>
2021-10-02 22:07:01 -0500

    AnimacionInicial

---
+++ NetworkJAm/Assets/Scripts/DialogueSystem/ChangeText.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeText : MonoBehaviour
+{
+    public Text texto;
+    [TextArea(3,8)]
+    public List<string> textos;
+    private void Start()
+    {
+    
+    }
+    public void ChangeTexto(int idTexto)
+    {
+        texto.text = textos[idTexto].ToString();
+    }
+}
