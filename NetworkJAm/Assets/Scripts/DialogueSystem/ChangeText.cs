52073faf706dc8c50881d77b1360d0ca53bb8571
RodoL0204 <rolega01@gmail.com>
2021-10-06 19:39:35 -0500

    lenguajes

--- NetworkJAm/Assets/Scripts/DialogueSystem/ChangeText.cs
+++ NetworkJAm/Assets/Scripts/DialogueSystem/ChangeText.cs
@@ -3,17 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+[System.Serializable]
+public struct Dialogos
+{
+    [TextArea(3, 8)]
+    public string[] textos;
+}
+
 public class ChangeText : MonoBehaviour
 {
+    
+    public Dialogos[] dialogo;
+
     public Text texto;
-    [TextArea(3,8)]
-    public List<string> textos;
+    //[TextArea(3,8)]
+    //public List<string> textos;
     private void Start()
     {
     
     }
     public void ChangeTexto(int idTexto)
     {
-        texto.text = textos[idTexto].ToString();
+        //texto.text = textos[idTexto].ToString();
+        texto.text = dialogo[PlayerPrefs.GetInt("lang")].textos[idTexto];
     }
 }
---
+++ NetworkJAm/Assets/Scripts/Idioma/SetLanguage.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetLanguage : MonoBehaviour
+{
+    [TextArea(3, 8)]
+    [SerializeField] string[] texts;
+    [SerializeField] Text text;
+
+    public void Start()
+    {
+        text.text = texts[PlayerPrefs.GetInt("lang")];
+    }
+    public void setL(int l)
+    {
+        PlayerPrefs.SetInt("lang", l);
+        foreach(SetLanguage s in FindObjectsOfType<SetLanguage>())
+        {
+            s.OverwriteL();
+        }
+    }
+
+    private void OnEnable()
+    {
+        text.text = texts[PlayerPrefs.GetInt("lang")];
+    }
+
+    public void OverwriteL()
+    {
+        text.text = texts[PlayerPrefs.GetInt("lang")];
+    }
+}
